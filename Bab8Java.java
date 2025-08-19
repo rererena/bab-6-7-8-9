ecc0d04aa20264a2ff74a043737393f8a1eae0fd
rererena <reremetaraena@gmail.com>
2025-08-19 13:16:25 +0700

    Add files via upload

---
+++ Bab8Java.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bab8.java;
+
+/**
+ *
+ * @author PC 10 - LAB R1
+ */
+public class Bab8Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
